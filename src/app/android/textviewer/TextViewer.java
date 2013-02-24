217e5cb88b025f2ed8708be1c387bd995e546d6b
alexesprit <alex.esprit@gmail.com>
2013-02-24 16:20:50 +0400

    Add progress dialog while file opening

--- src/app/android/textviewer/TextViewer.java
+++ src/app/android/textviewer/TextViewer.java
@@ -1,9 +1,12 @@
 package app.android.textviewer;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.SeekBar;
@@ -18,13 +21,41 @@ import java.io.IOException;
  *
  * @author esprit
  */
-public class TextViewer extends Activity {
+public final class TextViewer extends Activity {
+    private static final int ACTION_SHOW_PROGRESS = 0;
+    private static final int ACTION_HIDE_PROGRESS = 1;
+    private static final int ACTION_SET_CONTENT = 2;
+
     //private Uri currentUri;
-    
+    private Handler handler;
+
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.textviewer);
+
+        handler = new Handler() {
+            private ProgressDialog dialog = new ProgressDialog(TextViewer.this);
+
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case ACTION_SHOW_PROGRESS: {
+                        dialog = new ProgressDialog(TextViewer.this);
+                        dialog.setTitle(R.string.appName);
+                        dialog.setMessage(getBaseContext().getString(R.string.opening));
+                        dialog.show();
+                    } break;
+                    case ACTION_HIDE_PROGRESS: {
+                        dialog.dismiss();
+                    } break;
+                    case ACTION_SET_CONTENT: {
+                        String data[] = (String[])msg.obj;
+                        updateView(data[0], data[1]);
+                    } break;
+                }
+            }
+        };
 
         openFile(getIntent());
     }
@@ -40,14 +71,24 @@ public class TextViewer extends Activity {
         if (Intent.ACTION_VIEW.equals(action)) {
             Uri uri = intent.getData();
             if (null != uri) {
-                String path = uri.getPath();
-                String content = getFileContent(path);
-                if (null != content) {
-                    //currentUri = uri;
-                    updateView(path, content);
-                } else {
-                    showError(path);
-                }
+                final String path = uri.getPath();
+                Thread thread = new Thread() {
+                    @Override
+                    public void run() {
+                        handler.sendEmptyMessage(ACTION_SHOW_PROGRESS);
+                        String content = getFileContent(path);
+                        if (null != content) {
+                            //currentUri = uri;
+                            String data[] = new String[]{path, content};
+                            handler.sendMessage(handler.obtainMessage(ACTION_SET_CONTENT, data));
+                            handler.sendEmptyMessage(ACTION_HIDE_PROGRESS);
+                        } else {
+                            handler.sendEmptyMessage(ACTION_HIDE_PROGRESS);
+                            showError(path);
+                        }
+                    }
+                };
+                thread.start();
             }
         }
     }
@@ -88,5 +129,4 @@ public class TextViewer extends Activity {
         String error = String.format(template, path);
         Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
     }
-
 }
