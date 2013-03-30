74d3a09b7d64adc1c717c1cfa1ba9859440341d9
alexesprit <alex.esprit@gmail.com>
2013-03-30 19:30:49 +0400

    Set proper copyright

--- src/app/android/textviewer/TextViewer.java
+++ src/app/android/textviewer/TextViewer.java
@@ -1,17 +1,9 @@
 /*
- * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *                    Version 2, December 2004
+ * Copyright (c) 2013 esprit <alex.esprit@gmail.com>
  *
- * Copyright (c) esprit
- *
- * Everyone is permitted to copy and distribute verbatim or modified
- * copies of this license document, and changing it is allowed as long
- * as the name is changed.
- *
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
  */
 
 package app.android.textviewer;
